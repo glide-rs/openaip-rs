aed3ec4f06b65391f9cc9345434cdaba2a71c1ad
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-06-07 10:29:21 +0200

    error: Implement "Display" and "Error" traits

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,31 @@
+use std::error;
+use std::fmt;
+
 use xmltree;
 
 #[derive(Debug)]
 pub enum Error {
     Xml(xmltree::ParseError),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::Xml(ref err) => write!(f, "XML error: {}", err),
+        }
+    }
+}
+
+impl error::Error for Error {
+    fn description(&self) -> &str {
+        match *self {
+            Error::Xml(ref err) => err.description(),
+        }
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        match *self {
+            Error::Xml(ref err) => Some(err),
+        }
+    }
 }
