6973ca6ba73676a672a825aed8e2813663aa53fd
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-06-07 14:03:24 +0200

    AltitudeUnit: Add doc tests

--- src/altitude_unit.rs
+++ src/altitude_unit.rs
@@ -6,6 +6,16 @@ pub enum AltitudeUnit { Feet, FlightLevel }
 impl FromStr for AltitudeUnit {
     type Err = ();
 
+    /// # Examples
+    ///
+    /// ```
+    /// # use std::str::FromStr;
+    /// # use openaip_parser::AltitudeUnit;
+    /// #
+    /// assert_eq!(AltitudeUnit::from_str("F").unwrap(), AltitudeUnit::Feet);
+    /// assert_eq!(AltitudeUnit::from_str("FL").unwrap(), AltitudeUnit::FlightLevel);
+    /// assert!(AltitudeUnit::from_str("foobar").is_err());
+    /// ```
     fn from_str(s: &str) -> Result<Self, ()> {
         match s {
             "F" => Ok(AltitudeUnit::Feet),
