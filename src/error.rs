09e88119a66a9cb7c982ff283b8897498c3c0e70
Tobias Bieniek <tobias@bieniek.cloud>
2020-11-19 10:37:36 +0100

    Use `thiserror` to implement `Error` enum

--- src/error.rs
+++ src/error.rs
@@ -1,89 +1,25 @@
-#![allow(deprecated, bare_trait_objects)]
-
-use std::error;
-use std::fmt;
-use std::num;
-
-use xmltree;
-
-#[derive(Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum Error {
-    Xml(xmltree::ParseError),
-    InvalidIntNumber(num::ParseIntError),
-    InvalidFloatNumber(num::ParseFloatError),
+    #[error(transparent)]
+    Xml(#[from] xmltree::ParseError),
+    #[error(transparent)]
+    InvalidIntNumber(#[from] std::num::ParseIntError),
+    #[error(transparent)]
+    InvalidFloatNumber(#[from] std::num::ParseFloatError),
+    #[error("Missing <{0}> element")]
     MissingElement(&'static str),
+    #[error("Missing <{0}> attribute")]
     MissingAttribute(&'static str),
+    #[error("Missing element text")]
     MissingText,
+    #[error("Incompatible DATAFORMAT version: {0}")]
     IncompatibleDataFormatVersion(String),
+    #[error("Unknown airspace category: {0}")]
     UnknownCategory(String),
+    #[error("Unknown altitude reference: {0}")]
     UnknownAltitudeReference(String),
+    #[error("Unknown altitude unit: {0}")]
     UnknownAltitudeUnit(String),
+    #[error("Invalid point")]
     InvalidPoint,
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match *self {
-            Error::Xml(ref err) => err.fmt(f),
-            Error::InvalidIntNumber(ref err) => err.fmt(f),
-            Error::InvalidFloatNumber(ref err) => err.fmt(f),
-            Error::MissingElement(name) => write!(f, "Missing <{}> element", name),
-            Error::MissingAttribute(name) => write!(f, "Missing {} attribute", name),
-            Error::MissingText => write!(f, "Missing element text"),
-            Error::IncompatibleDataFormatVersion(ref version) => {
-                write!(f, "Incompatible DATAFORMAT version: {}", version)
-            }
-            Error::UnknownCategory(ref s) => write!(f, "Unknown airspace category: {}", s),
-            Error::UnknownAltitudeReference(ref s) => {
-                write!(f, "Unknown altitude reference: {}", s)
-            }
-            Error::UnknownAltitudeUnit(ref s) => write!(f, "Unknown altitude unit: {}", s),
-            Error::InvalidPoint => write!(f, "Invalid point"),
-        }
-    }
-}
-
-impl error::Error for Error {
-    fn description(&self) -> &str {
-        match *self {
-            Error::Xml(ref err) => err.description(),
-            Error::InvalidIntNumber(ref err) => err.description(),
-            Error::InvalidFloatNumber(ref err) => err.description(),
-            Error::MissingElement(..) => "Missing element",
-            Error::MissingAttribute(..) => "Missing attribute",
-            Error::MissingText => "Missing element text",
-            Error::IncompatibleDataFormatVersion(..) => "Incompatible DATAFORMAT version",
-            Error::UnknownCategory(..) => "Unknown airspace category",
-            Error::UnknownAltitudeReference(..) => "Unknown altitude reference",
-            Error::UnknownAltitudeUnit(..) => "Unknown altitude unit",
-            Error::InvalidPoint => "Invalid point",
-        }
-    }
-
-    fn cause(&self) -> Option<&error::Error> {
-        match *self {
-            Error::Xml(ref err) => Some(err),
-            Error::InvalidIntNumber(ref err) => Some(err),
-            Error::InvalidFloatNumber(ref err) => Some(err),
-            _ => None,
-        }
-    }
-}
-
-impl From<xmltree::ParseError> for Error {
-    fn from(err: xmltree::ParseError) -> Error {
-        Error::Xml(err)
-    }
-}
-
-impl From<num::ParseIntError> for Error {
-    fn from(err: num::ParseIntError) -> Error {
-        Error::InvalidIntNumber(err)
-    }
-}
-
-impl From<num::ParseFloatError> for Error {
-    fn from(err: num::ParseFloatError) -> Error {
-        Error::InvalidFloatNumber(err)
-    }
 }
